10bb67377e5a3b945d59154dbe81bbaf29944355
Nicholas Yoong <nicyoong321@gmail.com>
2024-11-04 21:46:53 +0800

    norminette friendly ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,12 +1,26 @@
-#include "../includes/libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 21:45:25 by nyoong            #+#    #+#             */
+/*   Updated: 2024/11/04 21:46:12 by nyoong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-size_t ft_strlen(const char *str) {
-    size_t length;
-    
-    length = 0;
-    while (*str) {
-        length++;
-        str++;
-    }
-    return length;
+#include "libft.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	length;
+
+	length = 0;
+	while (*str)
+	{
+		length++;
+		str++;
+	}
+	return (length);
 }
