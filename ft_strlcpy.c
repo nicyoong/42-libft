b8e7548dc876efb2b72166a395c4b2e6f8f2e252
Nicholas Yoong <nicyoong321@gmail.com>
2024-11-05 13:10:08 +0800

    norminette friendly ft strlcpy and ft strncmp

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nyoong <nyoong@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 11:46:49 by nyoong            #+#    #+#             */
+/*   Updated: 2024/11/05 11:46:50 by nyoong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stddef.h>
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	const char	*src_start = src;
-	size_t 		src_len;
-	size_t 		copy_index;
+	size_t		src_len;
+	size_t		copy_index;
 
 	src_len = 0;
 	while (*src++)
