f5e37020d1f539ee0f5e948ebd3f0d5cb6737e10
Nicholas Yoong <nicyoong321@gmail.com>
2024-11-05 13:37:13 +0800

    testing

--- libft.h
+++ libft.h
@@ -3,6 +3,7 @@
 
 # include <stddef.h>
 
+// Mandatory Part 1
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
@@ -19,9 +20,13 @@ int	ft_toupper(int c);
 int	ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int	ft_atoi(const char *str);
 void	*ft_calloc(size_t num, size_t size);
 char	*ft_strdup(const char *s1);
+
 
 # endif // LIBFT_H
