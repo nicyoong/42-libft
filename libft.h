ad86894a176b6fd218f0342b06b22ac1aa46e7e4
Nicholas Yoong <nicyoong321@gmail.com>
2024-11-05 16:42:39 +0800

    ft_split.c

--- libft.h
+++ libft.h
@@ -31,7 +31,7 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-// split
+char	**ft_split(const char *s, char c);
 // itoa
 // strmapi
 // striteri
