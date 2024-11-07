70ea8c6c4d15e5e0f28824d0b1ae393d6c497d0c
Nic <nicyoong321@gmail.com>
2024-11-07 10:52:43 +0800

    Fixing small mistakes
    I messed up these two but I will fix them now.

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -24,6 +24,8 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	src_len = 0;
 	while (dest[dest_len] && dest_len < size)
 		dest_len++;
+	if (size == 0)
+		return (src_len);	
 	while (src[src_len])
 		src_len++;
 	if (size <= dest_len)
